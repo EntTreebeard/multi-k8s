c82f7afc2ae6455c4237d3c88ddd2eaa242c0e7e
EntTreebeard <uksonal@gmail.com>
2020-05-24 15:16:29 +0300

    deploy4

--- deploy.sh
+++ deploy.sh
@@ -2,12 +2,14 @@ docker build -t agacsakal/multi-client:latest -t agacsakal/multi-client:$SHA -f 
 docker build -t agacsakal/multi-server:latest -t agacsakal/multi-server:$SHA -f ./server/Dockerfile ./server
 docker build -t agacsakal/multi-worker:latest -t agacsakal/multi-worker:$SHA -f ./worker/Dockerfile ./worker
 
+
 docker push agacsakal/multi-client:latest
 docker push agacsakal/multi-server:latest
 docker push agacsakal/multi-worker:latest
 docker push agacsakal/multi-client:$SHA
 docker push agacsakal/multi-server:$SHA
 docker push agacsakal/multi-worker:$SHA
+
 
 kubectl apply -f k8s
 kubectl set image deployments/server-deployment server=stephengrider/multi-server:$SHA
